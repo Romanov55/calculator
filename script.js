872667aea64ca33f320661eea4f3ecdebd8b1490
Romanov55 <Romanov20let@gmail.com>
2022-11-29 14:53:11 +0600

    add script

---
+++ script.js
@@ -0,0 +1,90 @@
+function plus (){
+    const operator = '+';
+    document.getElementById('result').innerHTML += operator;
+
+}
+
+function minus (){
+    const operator = '-';
+    document.getElementById('result').innerHTML += operator;
+}
+
+function division (){
+    const operator = '÷';
+    document.getElementById('result').innerHTML += operator;
+}
+
+function multiply (){
+    const operator = '*';
+    document.getElementById('result').innerHTML += operator;
+}
+
+function del (){
+    const operator = 0;
+    document.getElementById('result').innerHTML = operator;
+}
+
+function comma (){
+    const mark = ',';
+    document.getElementById('result').innerHTML += mark;
+}
+
+function zero (){
+    const num = 0;
+    document.getElementById('result').innerHTML += num;
+}
+
+function one (){
+    const num = 1;
+    document.getElementById('result').innerHTML += num;
+}
+
+function two (){
+    const num = 2;
+    document.getElementById('result').innerHTML += num;
+}
+
+function three (){
+    const num = 3;
+    document.getElementById('result').innerHTML += num;
+}
+
+function four (){
+    const num = 4;
+    document.getElementById('result').innerHTML += num;
+}
+
+function five (){
+    const num = 5;
+    document.getElementById('result').innerHTML += num;
+}
+
+function six (){
+    const num = 6;
+    document.getElementById('result').innerHTML += num;
+}
+
+function seven (){
+    const num = 7;
+    document.getElementById('result').innerHTML += num;
+}
+
+function eight (){
+    const num = 8;
+    document.getElementById('result').innerHTML += num;
+}
+
+function nine (){
+    const num = 9;
+    document.getElementById('result').innerHTML += num;
+}
+
+function plus (){
+    document.getElementById('result').innerHTML += '+';
+}
+
+function alert (){
+    if (document.getElementById('result') === '0') {
+        alert(yes)
+    }
+}
