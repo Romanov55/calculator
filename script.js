2fe6bbed2f4dc9656d3dde0044c0e37f92c0487b
Romanov55 <Romanov20let@gmail.com>
2022-11-30 13:32:15 +0600

    up script

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 function plus (){
     const operator = '+';
-    document.getElementById('result').innerHTML += operator;
-
+    let str = document.getElementById('result').innerHTML
+    if (str.slice(-1) === '+' || str.slice(-1) === '-' || str.slice(-1) === '*' || str.slice(-1) === '÷') {
+        document.getElementById('result').innerHTML = `${str.slice(0,-1)}${operator}`
+    } else if (str.slice(-1) !== ',' && str.length < 19) {
+        document.getElementById('result').innerHTML += operator;
+    }
 }
 
 function minus (){
     const operator = '-';
-    document.getElementById('result').innerHTML += operator;
+    let str = document.getElementById('result').innerHTML
+    if (str.slice(-1) === '+' || str.slice(-1) === '-' || str.slice(-1) === '*' || str.slice(-1) === '÷') {
+        document.getElementById('result').innerHTML = `${str.slice(0,-1)}${operator}`
+    } else if (str.slice(-1) !== ',' && str.length < 19) {
+        document.getElementById('result').innerHTML += operator;
+    }
 }
 
 function division (){
-    const operator = '÷';
-    document.getElementById('result').innerHTML += operator;
+    const operator = '/';
+    let str = document.getElementById('result').innerHTML
+    if (str.slice(-1) === '+' || str.slice(-1) === '-' || str.slice(-1) === '*' || str.slice(-1) === '÷') {
+        document.getElementById('result').innerHTML = `${str.slice(0,-1)}${operator}`
+    } else if (str.slice(-1) !== ',' && str.length < 19) {
+        document.getElementById('result').innerHTML += operator;
+    }
 }
 
 function multiply (){
     const operator = '*';
-    document.getElementById('result').innerHTML += operator;
+    let str = document.getElementById('result').innerHTML
+    if (str.slice(-1) === '+' || str.slice(-1) === '-' || str.slice(-1) === '*' || str.slice(-1) === '÷') {
+        document.getElementById('result').innerHTML = `${str.slice(0,-1)}${operator}`
+    } else if (str.slice(-1) !== ',' && str.length < 19) {
+        document.getElementById('result').innerHTML += operator;
+    }
 }
 
 function del (){
@@ -24,93 +43,133 @@ function del (){
     document.getElementById('result').innerHTML = operator;
 }
 
+function cancel (){
+    let str = document.getElementById('result').innerHTML;
+    if (str.length === 1) {
+        document.getElementById('result').innerHTML = '0';
+    } else {
+        document.getElementById('result').innerHTML = str.slice(0,-1);
+    }
+}
+
 function comma (){
-    const mark = ',';
-    document.getElementById('result').innerHTML += mark;
+    const mark = '.';
+    let str = document.getElementById('result').innerHTML;
+    if (str.slice(-1) !== '+' && str.slice(-1) !== '-' && str.slice(-1) !== '*' && str.slice(-1) !== '÷') {
+        document.getElementById('result').innerHTML += mark;
+    }
 }
 
 function zero (){
     const num = 0;
-    document.getElementById('result').innerHTML += num;
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML !== '0') {
+        document.getElementById('result').innerHTML += num;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML = num;
+    }
 }
 
 function one (){
     const symbol = '1';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function two (){
     const symbol = '2';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function three (){
     const symbol = '3';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function four (){
     const symbol = '4';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function five (){
     const symbol = '5';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function six (){
     const symbol = '6';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function seven (){
     const symbol = '7';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function eight (){
     const symbol = '8';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
 }
 
 function nine (){
     const symbol = '9';
-    if (document.getElementById('result').innerHTML !== '0') {
-        document.getElementById('result').innerHTML += symbol;
-    } else {
+    let str = document.getElementById('result').innerHTML;
+    if (document.getElementById('result').innerHTML === '0') {
         document.getElementById('result').innerHTML = symbol;
+    } else if (str.length < 20) {
+        document.getElementById('result').innerHTML += symbol;
     }
+}
+
+function answer (){
+    let str = document.getElementById('result').innerHTML;
+    document.getElementById('small-result').innerHTML = eval(str)
+    document.getElementById('result').innerHTML = eval(str)
+}
+
+function radical (){
+    let str = document.getElementById('result').innerHTML;
+    const res = str.split(/(\+|\-|\*|\÷)/)
+    const begin = res.slice(0,-1).join('')
+    document.getElementById('small-result').innerHTML = Math.sqrt(res[res.length-1])
+    document.getElementById('result').innerHTML = `${begin}${Math.sqrt(res[res.length-1])}`
 }
